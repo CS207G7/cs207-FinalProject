ed1565f36baad3cf486799df32c60e85d57506f8
Benjamin Rafetto <brafetto@g.harvard.edu>
2017-10-15 14:21:30 -0400

    Adding test folder along with several good and bad xmls.

---
+++ test/test_XML_reader.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../src')
+
+import chemkin
+
+def test_XML_bad_reactants():
+	raise NotImplementedError()
+
+def test_XML_bad_products():
+	raise NotImplementedError()
+
+test_XML_bad_reactants()
+test_XML_bad_products()
