cfe455c3d8767e8474acfff82273e81d404e5561
Peng, Xuefeng <xpeng@g.harvard.edu>
2017-10-15 13:10:19 -0400

    updated chemkin.py

--- src/chemkin.py
+++ src/chemkin.py
@@ -50,114 +50,146 @@ class inputReader:
 				'v2': v2
 			}
 
-class reaction_coeff:
+class Reaction:
 
-	def constant(self, k):
-		"""Returns the constant reaction coeff
-    
-	    INPUTS
-	    =======
-	    k: float, the constant reaction coeff
+	class reaction_coeff:
+		
+		@classmethod
+		def constant(self, k):
+			"""Returns the constant reaction coeff
 	    
-	    RETURNS
-	    ========
-	    k: float, the constant reaction coeff
-	    
-	    EXAMPLES
-	    =========
-	    >>> const(1.0)
-	    1.0
-	    >>> const(3.773)
-	    3.773
-	    """
-		return k
+		    INPUTS
+		    =======
+		    k: float, the constant reaction coeff
+		    
+		    RETURNS
+		    ========
+		    k: float, the constant reaction coeff
+		    
+		    EXAMPLES
+		    =========
+		    >>> const(1.0)
+		    1.0
+		    >>> const(3.773)
+		    3.773
+		    """
+			return k
 
-	def arr(self, A, E, T, R=8.314):
-	    """Returns the Arrhenius reaction rate coeff
-	    
-	    INPUTS
-	    =======
-	    A: positive float, Arrhenius prefactor
-	    E: float, activation energy for the reaction
-	    T: float, temperature in Kelvin scale
-	    
-	    RETURNS
-	    ========
-	    coeff: float, the Arrhenius reaction coeff
-	    
-	    EXAMPLES
-	    =========
-	    >>> arr(10**7, 10**3, 10**2)
-	    3003549.0889639612
-	    """
-	    # Check type for all args
-	    if type(A) is not int and type(A) is not float:
-	        raise TypeError("The Arrhenius prefactor A should be either int or float")
-	    if type(E) is not int and type(E) is not float:
-	        raise TypeError("Activation Energy should be either int or float")
-	    if type(T) is not int and type(T) is not float:
-	        raise TypeError("Temperature (in Kelvin scale) should be either int or float")
-	    
-	    # Check under/over flow
-	    if A >= float('inf') or A <= float('-inf'):
-	        raise ValueError("The Arrhenius prefactor A is under/overflow")
-	    if E >= float('inf') or E <= float('-inf'):
-	        raise ValueError("Activation Energy E is under/overflow")
-	    if T >= float('inf') or T <= float('-inf'):
-	        raise ValueError("Temperature T is under/overflow")
-	        
-	    # Check value requirements
-	    if A <= 0:
-	        raise ValueError("The Arrhenius prefactor A is strictly positive")
-	    if T < 0:
-	        raise ValueError("Temperature in Kelvin scale should be positive")
-	    
-	    return A * np.exp( - E / (R * T) )
+		@classmethod
+		def arr(self, A, E, T, R=8.314):
+			"""Returns the Arrhenius reaction rate coeff
+		    
+			INPUTS
+		    =======
+		    A: positive float, Arrhenius prefactor
+		    E: float, activation energy for the reaction
+		    T: float, temperature in Kelvin scale
+		    
+		    RETURNS
+		    ========
+		    coeff: float, the Arrhenius reaction coeff
+		    
+		    EXAMPLES
+		    =========
+		    >>> arr(10**7, 10**3, 10**2)
+		    3003549.0889639612
+		    """
+		    # Check type for all args
+			if type(A) is not int and type(A) is not float:
+				raise TypeError("The Arrhenius prefactor A should be either int or float")
+			if type(E) is not int and type(E) is not float:
+				raise TypeError("Activation Energy should be either int or float")
+			if type(T) is not int and type(T) is not float:
+				raise TypeError("Temperature (in Kelvin scale) should be either int or float")
+		    
+			# Check under/over flow
+			if A >= float('inf') or A <= float('-inf'):
+				raise ValueError("The Arrhenius prefactor A is under/overflow")
+			if E >= float('inf') or E <= float('-inf'):
+				raise ValueError("Activation Energy E is under/overflow")
+			if T >= float('inf') or T <= float('-inf'):
+				raise ValueError("Temperature T is under/overflow")
+		        
+			# Check value requirements
+			if A <= 0:
+				raise ValueError("The Arrhenius prefactor A is strictly positive")
+			if T < 0:
+				raise ValueError("Temperature in Kelvin scale should be positive")
+			return A * np.exp( - E / (R * T) )
 
-	def mod_arr(self, A, b, E, T, R=8.314):
-	    """Returns the modified Arrhenius reaction rate coeff
-	    
-	    INPUTS
-	    =======
-	    A: positive float, Arrhenius prefactor
-	    b: real number, The modified Arrhenius parameter
-	    E: float, activation energy for the reaction
-	    T: float, temperature in Kelvin scale
-	    
-	    RETURNS
-	    ========
-	    coeff: float, the Arrhenius reaction coeff
-	    
-	    EXAMPLES
-	    =========
-	    >>> mod_arr(10**7, 0.5, 10**3, 10**2)
-	    30035490.889639609
-	    """
-	    # Check type for all args
-	    if type(A) is not int and type(A) is not float:
-	        raise TypeError("The Arrhenius prefactor A should be either int or float")
-	    if type(b) is not int and type(b) is not float:
-	        raise TypeError("The modified Arrhenius parameter b should be either int or float")
-	    if type(E) is not int and type(E) is not float:
-	        raise TypeError("Activation Energy E should be either int or float")
-	    if type(T) is not int and type(T) is not float:
-	        raise TypeError("Temperature (in Kelvin scale) T should be either int or float")
-	    
-	    # Check under/over flow
-	    if A >= float('inf') or A <= float('-inf'):
-	        raise ValueError("The Arrhenius prefactor A is under/overflow")
-	    if b >= float('inf') or b <= float('-inf'):
-	        raise ValueError("The modified Arrhenius parameter b is under/overflow")
-	    if E >= float('inf') or E <= float('-inf'):
-	        raise ValueError("Activation Energy E is under/overflow")
-	    if T >= float('inf') or T <= float('-inf'):
-	        raise ValueError("Temperature T is under/overflow")
-	        
-	    # Check value requirements
-	    if A <= 0:
-	        raise ValueError("The Arrhenius prefactor A is strictly positive")
-	    if T < 0:
-	        raise ValueError("Temperature in Kelvin scale should be positive")
-	    
-	    return A * (T**b) * np.exp( - E / (R * T) )
+		@classmethod
+		def mod_arr(self, A, b, E, T, R=8.314):
+			"""Returns the modified Arrhenius reaction rate coeff
 
+			INPUTS
+			=======
+			A: positive float, Arrhenius prefactor
+			b: real number, The modified Arrhenius parameter
+			E: float, activation energy for the reaction
+			T: float, temperature in Kelvin scale
+			RETURNS
+			========
+			coeff: float, the Arrhenius reaction coeff
+			
+			EXAMPLES
+			=========
+			>>> mod_arr(10**7, 0.5, 10**3, 10**2)
+			30035490.889639609
+			"""
+			# Check type for all args
+			if type(A) is not int and type(A) is not float:
+				raise TypeError("The Arrhenius prefactor A should be either int or float")
+			if type(b) is not int and type(b) is not float:
+				raise TypeError("The modified Arrhenius parameter b should be either int or float")
+			if type(E) is not int and type(E) is not float:
+				raise TypeError("Activation Energy E should be either int or float")
+			if type(T) is not int and type(T) is not float:
+				raise TypeError("Temperature (in Kelvin scale) T should be either int or float")
+
+			# Check under/over flow
+			if A >= float('inf') or A <= float('-inf'):
+				raise ValueError("The Arrhenius prefactor A is under/overflow")
+			if b >= float('inf') or b <= float('-inf'):
+				raise ValueError("The modified Arrhenius parameter b is under/overflow")
+			if E >= float('inf') or E <= float('-inf'):
+				raise ValueError("Activation Energy E is under/overflow")
+			if T >= float('inf') or T <= float('-inf'):
+				raise ValueError("Temperature T is under/overflow")
+				# Check value requirements
+			if A <= 0:
+				raise ValueError("The Arrhenius prefactor A is strictly positive")
+			if T < 0:
+				raise ValueError("Temperature in Kelvin scale should be positive")
+			return A * (T**b) * np.exp( - E / (R * T) )
+
+	@classmethod
+	def reaction_rate(self, v1, v2, x, k):
+		"""Returns the reaction rate for a system
+		INPUTS
+		=======
+		v1: 2 x 3 float vector
+		v2: 2 x 3 float vector
+		x: 2 x 3 float vector, species concentrations
+		k: 2 x 1 float/int list
+
+		RETURNS
+		========
+		reaction rates: 1x3 float array, the reaction rate for each specie
+
+		EXAMPLES
+		=========
+		>>> rr([[1,2,0],[0,0,2]], [[0,0,1],[1,2,0]], [1,2,1], [10,10])
+		array([-30, -60,  20])
+		"""
+		try:
+			x = np.array(x)
+			v1 = np.array(v1)
+			v2 = np.array(v2)
+			k = np.array(k)
+		except TypeError as err:
+			raise TypeError("x and v should be either int or float vectors")
+		if k.any() < 0:
+			raise ValueError("reaction constant can't be negative")
+		ws = k * np.prod(x ** v1, axis=1)
+		rrs = np.sum((v2 - v1) * np.array([ws]).T, axis=0)
+		return rrs
