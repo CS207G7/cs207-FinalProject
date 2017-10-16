841eac126a65d0faf500f7c1e9af917b8a20b59c
Benjamin Rafetto <brafetto@g.harvard.edu>
2017-10-16 18:32:32 -0400

    Adding a working unit test so I can check travis and coveralls.

--- test/test_XML_reader.py
+++ test/test_XML_reader.py
@@ -9,5 +9,13 @@ def test_XML_bad_reactants():
 def test_XML_bad_products():
 	raise NotImplementedError()
 
-test_XML_bad_reactants()
-test_XML_bad_products()
+def test_XML_reader_1():
+	r_reader = chemkin.ReactionParser('xml/xml_good1.xml')
+	#print(r_reader)
+	expected = ['H', 'O', 'OH', 'H2', 'O2']
+	assert r_reader.get_species() == expected, "ReactionParser getting incorrect species."
+
+test_XML_reader_1()
+
+#test_XML_bad_reactants()
+#test_XML_bad_products()
