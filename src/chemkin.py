1f116b689d3050f8b0505d579a272dd21e1ab572
Peng, Xuefeng <xpeng@g.harvard.edu>
2017-10-16 15:10:18 -0400

    make chemkin more robust, added main blockfor testing

--- src/chemkin.py
+++ src/chemkin.py
@@ -1,7 +1,8 @@
 import numpy as np
 import xml.etree.ElementTree as ET
+from enum import Enum
 
-class inputReader:
+class ReactionParser:
 
 	def __init__(self, input_path):
 		try:
@@ -9,9 +10,6 @@ class inputReader:
 		except ValueError as err:
 			raise ValueError('Something went wrong with your XML file:\n' + str(err))
 		self.root = tree.getroot()
-		self.reactions = {}
-		self.parse_reactions()
-
 
 	def get_species(self):
 		species = self.root.find('phase').find('speciesArray')
@@ -20,26 +18,36 @@ class inputReader:
 		return species
 
 	def parse_reactions(self):
+		reaction_dict = {}
 		reactions = self.root.find('reactionData').findall('reaction')
+		
 		for i, reaction in enumerate(reactions):
 			attributes = reaction.attrib
 			reversible, rtype, rid = attributes['reversible'], attributes['type'], attributes['id']
 			# Equation
 			equation = reaction.find('equation').text
 			# Arrhenius Params
-			coeffs = reaction.find('rateCoeff').find('Arrhenius')
-			A, E =  float(coeffs.find('A').text), float(coeffs.find('E').text)
-			if coeffs.find('b'):
-				arr_params = {'A': A, 'E': E, 'b': float(coeffs.find('b').text)}
-			else:
-				arr_params = {'A': A, 'E': E}
+			const_coeff = reaction.find('rateCoeff').find('Constant')
 
-			# Concentration
-			## TODO: probably need to deal with wrong input format
+			if const_coeff:
+				coeff_params = {'k': const_coeff}
+			else:
+				coeffs = reaction.find('rateCoeff').find('Arrhenius')
+				try:
+					A, E =  float(coeffs.find('A').text), float(coeffs.find('E').text)
+				except:
+					raise ValueError('did not capture Arrhenius prefactor A and activation energy E, please re-check input format')
+				
+				if coeffs.find('b'):
+					coeff_params = {'A': A, 'E': E, 'b': float(coeffs.find('b').text)}
+				else:
+					coeff_params = {'A': A, 'E': E}
+
 			reactants = reaction.find('rateCoeff').find('reactants').text.split(' ')
 			products = reaction.find('rateCoeff').find('products').text.split(' ')
 			# x = np.zeros( (len(reactants) + len(products), 1) )
 			v1, v2 = np.zeros( ( len(reactants) + len(products), 1 ) ), np.zeros( ( len(reactants) + len(products), 1 ) )
+			
 			for i in range( len(reactants) ):
 				# check format, e.g. H:1, not H:1O:2
 				if reactants[i].count(':') is not 1:
@@ -50,17 +58,19 @@ class inputReader:
 					raise ValueError('check your products input format: ' + products)
 				v2[i + j + 1] = products[j].split(':')[1]
 
-			self.reactions[rid] = {
+			reaction_dict[rid] = {
 				'type': rtype,
 				'reversible': reversible,
 				'equation': equation,
 				'species': self.get_species(),
-				'coeff_params': arr_params,
+				'coeff_params': coeff_params,
 				'v1': v1,
 				'v2': v2
 			}
+			return reaction_dict
 
-class Reaction:
+
+class ChemKin:
 
 	class reaction_coeff:
 		
@@ -86,13 +96,15 @@ class Reaction:
 			return k
 
 		@classmethod
-		def arr(self, A, E, T, R=8.314):
+		def arr(self, **kwargs, T, R=8.314):
 			"""Returns the Arrhenius reaction rate coeff
 		    
 			INPUTS
 		    =======
+		    kwargs:
 		    A: positive float, Arrhenius prefactor
 		    E: float, activation energy for the reaction
+
 		    T: float, temperature in Kelvin scale
 		    
 		    RETURNS
@@ -104,6 +116,7 @@ class Reaction:
 		    >>> arr(10**7, 10**3, 10**2)
 		    3003549.0889639612
 		    """
+		    A, E = kwargs['A'], kwargs['E']
 		    # Check type for all args
 			if type(A) is not int and type(A) is not float:
 				raise TypeError("The Arrhenius prefactor A should be either int or float")
@@ -128,15 +141,18 @@ class Reaction:
 			return A * np.exp( - E / (R * T) )
 
 		@classmethod
-		def mod_arr(self, A, b, E, T, R=8.314):
+		def mod_arr(self, **kwargs, T, R=8.314):
 			"""Returns the modified Arrhenius reaction rate coeff
 
 			INPUTS
 			=======
+			kwargs
 			A: positive float, Arrhenius prefactor
 			b: real number, The modified Arrhenius parameter
 			E: float, activation energy for the reaction
+			
 			T: float, temperature in Kelvin scale
+			
 			RETURNS
 			========
 			coeff: float, the Arrhenius reaction coeff
@@ -146,6 +162,8 @@ class Reaction:
 			>>> mod_arr(10**7, 0.5, 10**3, 10**2)
 			30035490.889639609
 			"""
+			A, E, b = kwargs['A'], kwargs['E'], kwargs['b']
+			
 			# Check type for all args
 			if type(A) is not int and type(A) is not float:
 				raise TypeError("The Arrhenius prefactor A should be either int or float")
@@ -180,7 +198,7 @@ class Reaction:
 		v1: 2 x 3 float vector
 		v2: 2 x 3 float vector
 		x: 2 x 3 float vector, species concentrations
-		k: 2 x 1 float/int list
+		k: 2 x 1 float/int list, coeffs
 
 		RETURNS
 		========
@@ -203,3 +221,54 @@ class Reaction:
 		ws = k * np.prod(x ** v1, axis=1)
 		rrs = np.sum((v2 - v1) * np.array([ws]).T, axis=0)
 		return rrs
+
+class Reaction:
+
+	def __init__(self, reactions):
+		self.reactions = reactions
+
+	def __repr__(self):
+		reaction_str = ''
+		for reaction in self.reactions:
+			reaction_str += (str(reaction) + '\n')
+
+		return 'Reactions:\n---------------\n' + reaction_str
+
+	def __len__(self):
+		return len(self.reactions)
+
+	def reaction_components(self):
+
+		V1 = np.zeros((len(self.reactions, self.reactions['v1'].shape[1])))
+		V2 = np.zeros((len(self.reactions, self.reactions['v2'].shape[1])))
+		for i, reaction in enumerate(self.reactions):
+			V1[i] = self.reactions['v1']
+			V2[i] = self.reactions['v2']
+
+		return V1, V2
+
+	def reaction_coeff_params(self, T):
+		
+		coeffs = []
+		for i, reaction in enumerate(self.reactions):
+			if hasattr(reaction['coeff_params'], 'k'):
+				# constant, T is ignored
+				coeffs.append( ChemKin.reaction_rate.constant(reaction['coeff_params']['k']) )
+			elif hasattr(reaction['coeff_params'], 'b'):
+				# modified 
+				coeffs.append( ChemKin.reaction_rate.mod_arr(reaction['coeff_params'], T) )
+			else:
+				coeffs.append( ChemKin.reaction_rate.arr(reaction['coeff_params'], T) )
+
+		return coeffs
+			
+
+if __name__ == "__main__":
+
+	T = 750
+	X = [2, 1, 0.5, 1, 1]
+	reactions = Reaction(ReactionParser('rxns.xml').parse_reactions())
+	print (reactions)
+	V1, V2 = reactions.reaction_components()
+	k = reactions.reaction_coeff_params(T)
+	print ( ChemKin.reaction_rate(V1, V2, X, k) )
