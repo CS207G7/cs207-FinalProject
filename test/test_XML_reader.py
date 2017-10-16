ae5a875abe892d1696e7a41ec8345a1e225e677e
Benjamin Rafetto <brafetto@g.harvard.edu>
2017-10-16 19:16:59 -0400

    Adding more xml tests and removing old roots files.

--- test/test_XML_reader.py
+++ test/test_XML_reader.py
@@ -3,19 +3,25 @@ sys.path.append('../src')
 
 import chemkin
 
-def test_XML_bad_reactants():
-	raise NotImplementedError()
-
-def test_XML_bad_products():
-	raise NotImplementedError()
-
 def test_XML_reader_1():
 	r_reader = chemkin.ReactionParser('xml/xml_good1.xml')
-	#print(r_reader)
 	expected = ['H', 'O', 'OH', 'H2', 'O2']
 	assert r_reader.get_species() == expected, "ReactionParser getting incorrect species."
 
-test_XML_reader_1()
+def test_XML_bad_reactants():
+    r_reader = chemkin.ReactionParser('xml/xml_bad_reactants.xml')
+    try:
+        reactions = r_reader.parse_reactions()
+    except Exception as err:
+        assert type(err) == TypeError, "Bad reactants should raise a TypeError."
 
-#test_XML_bad_reactants()
-#test_XML_bad_products()
+def test_XML_bad_products():
+    r_reader = chemkin.ReactionParser('xml/xml_bad_products.xml')
+    try:
+        reactions = r_reader.parse_reactions()
+    except Exception as err:
+        assert type(err) == TypeError, "Bad products should raise a TypeError."
+
+test_XML_reader_1()
+test_XML_bad_reactants()
+test_XML_bad_products()
