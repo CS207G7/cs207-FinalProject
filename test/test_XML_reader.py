1a06796828d022ce6fdd5b4d3ec165e0c78242b8
Benjamin Rafetto <brafetto@g.harvard.edu>
2017-10-18 00:59:09 -0400

    Fixing sys path.

--- test/test_XML_reader.py
+++ test/test_XML_reader.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append('../src')
-#sys.path.append('src')
+#sys.path.append('../src')
+sys.path.append('src')
 #sys.path.append('test')
 #sys.path.append('test/xml')
 
@@ -26,7 +26,7 @@ def test_XML_bad_products():
         assert type(err) == ValueError, "Bad products should raise a TypeError."
 
 def test_XML_reader_2_V1():
-    r_reader = chemkin.ReactionParser('xml/xml_good2.xml')
+    r_reader = chemkin.ReactionParser('test/xml/xml_good2.xml')
     reactions = r_reader.parse_reactions()
     chem = chemkin.Reaction(r_reader)
     V1, V2 = chem.reaction_components()
@@ -57,6 +57,6 @@ def test_XML_reader_2_V1():
 #test_XML_bad_products()
 
 #test_XML_homework()
-test_XML_reader_2_V1()
+#test_XML_reader_2_V1()
 #test_XML_reader_2_V2()
 
