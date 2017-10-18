f9736f56764dc37ef9cde7eec44fd6db2b06c18e
Peng, Xuefeng <xpeng@g.harvard.edu>
2017-10-18 01:39:56 -0400

    remove dot dot in testing file

--- test/test_XML_reader.py
+++ test/test_XML_reader.py
@@ -1,6 +1,6 @@
 import sys
 #sys.path.append('../src')
-sys.path.append('../src')
+sys.path.append('src')
 #sys.path.append('test')
 #sys.path.append('test/xml')
 
@@ -27,7 +27,7 @@ def test_XML_bad_products():
         assert type(err) == ValueError, "Bad products should raise a TypeError."
 
 def test_XML_reader_2_V1():
-    r_reader = chemkin.ReactionParser('../test/xml/xml_good2.xml')# chemkin.ReactionParser('test/xml/xml_good2.xml')
+    r_reader = chemkin.ReactionParser('test/xml/xml_good2.xml')# chemkin.ReactionParser('test/xml/xml_good2.xml')
     reactions = r_reader.parse_reactions()
     chem = chemkin.Reaction(r_reader)
     V1, V2 = chem.reaction_components()
