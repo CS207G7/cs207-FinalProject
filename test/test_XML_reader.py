8bd0f6a1c18d38561de13bd9be1d38552710c270
Peng, Xuefeng <xpeng@g.harvard.edu>
2017-10-18 01:28:39 -0400

    did the xml homework test to see if work

--- test/test_XML_reader.py
+++ test/test_XML_reader.py
@@ -1,10 +1,11 @@
 import sys
 #sys.path.append('../src')
-sys.path.append('src')
+sys.path.append('../src')
 #sys.path.append('test')
 #sys.path.append('test/xml')
 
 import chemkin
+import numpy as np
 
 def test_XML_reader_1():
 	r_reader = chemkin.ReactionParser('test/xml/xml_good1.xml')
@@ -26,7 +27,7 @@ def test_XML_bad_products():
         assert type(err) == ValueError, "Bad products should raise a TypeError."
 
 def test_XML_reader_2_V1():
-    r_reader = chemkin.ReactionParser('test/xml/xml_good2.xml')
+    r_reader = chemkin.ReactionParser('test/xml/xml_good2.xml')# chemkin.ReactionParser('test/xml/xml_good2.xml')
     reactions = r_reader.parse_reactions()
     chem = chemkin.Reaction(r_reader)
     V1, V2 = chem.reaction_components()
@@ -42,21 +43,23 @@ def test_XML_reader_2_V1():
 #    assert all(V2 == [[0], [1], [1], [0], [0]]), "XML Reader loading incorrect v1 in file xml_good2.xml"
 
 #Not yet implemented
-#def test_XML_homework():
-    #r_reader = chemkin.ReactionParser('test/xml/xml_homework.xml')
-    #reactions = r_reader.parse_reactions()
-    #reaction1 = reactions['reaction01']
+def test_XML_homework():
+    r_reader = chemkin.ReactionParser('test/xml/xml_homework.xml')
+    reactions = r_reader.parse_reactions()
 
-    #chem = chemkin.Reaction(r_reader)
-    #print(repr(chem))
-    #chem.reaction_coeff_params()
-    #V1, V2 = chem.reaction_components()
+    chem = chemkin.Reaction(r_reader)
+    T = 750
+    X = [2, 1, 0.5, 1, 1]
+    V1, V2 = chem.reaction_components()
+    k = chem.reaction_coeff_params(T)
+    rrs = chemkin.ChemKin.reaction_rate(V1, V2, X, k)
+    ans = np.array([-3607077.87280406, -5613545.18362079, 9220623.05642485, 2006467.31081673,-2006467.31081673])
+    assert (rrs - ans).all() < 0.000000001
 
 #test_XML_reader_1()
 #test_XML_bad_reactants()
 #test_XML_bad_products()
-
 #test_XML_homework()
-#test_XML_reader_2_V1()
+# test_XML_reader_2_V1()
 #test_XML_reader_2_V2()
 
