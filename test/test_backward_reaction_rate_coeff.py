62a61c3b9f130513c720dd3217d4788c51e64cd6
Andrea Porelli <anp398@g.harvard.edu>
2017-11-19 15:37:27 -0500

    some tests on backward reaction

--- test/test_backward_reaction_rate_coeff.py
+++ test/test_backward_reaction_rate_coeff.py
@@ -1,48 +1,72 @@
-import sys
-sys.path.append('src')
-import chemkin
+#import sys
+# sys.path.append('src')
+from src import chemkin
+import numpy as np
 
-#Function: Cp_over_R
+#Function: H_over_RT
 
-## testing for correct values
-def test_Cp_over_R_result():
-    H_O2 = np.array([[2.50000001, -2.30842973e-11, 1.61561948e-14, -4.73515235e-18, 4.98197357e-22, 25473.6599, -0.44668291],/
-                     [3.28253784, 0.00148308754, -7.57966669e-07, 2.09470555e-10, -2.16717794e-14, -1088.45772, 5.4532312]]).T
+# testing for correct values
+
+reactions = chemkin.Reaction(chemkin.ReactionParser('../test/xml/xml_homework.xml'), T=3500)
+
+
+def test_H_over_RT_result():
+    H_O2 = np.array([[2.50000001, -2.30842973e-11, 1.61561948e-14, -4.73515235e-18, 4.98197357e-22, 25473.6599, -0.44668291], [3.28253784, 0.00148308754, -7.57966669e-07, 2.09470555e-10, -2.16717794e-14, -1088.45772, 5.4532312]]).T
+
     Tmax = 3500.000
-    compare = np.array([2.4999999988594293, 4.9171811841624997])
-    output = thermochem.Cp_over_R(H_O2,Tmax)
+    compare = np.array([3.69508857, 4.06676078, 5.08138715, 6.28213553, -2.93437561])
+    reactions.get_nasa_coeffs = H_O2
+    output = reactions.H_over_RT()
     assert np.equal(output, compare).all() == 0, 'Expected outcome'
 
-def test_Cp_over_R_bad_result():
-    H_O2 = np.array([[2.50000001, -2.30842973e-11, 1.61561948e-14, -4.73515235e-18, 4.98197357e-22, 25473.6599, -0.44668291],/
-                     [3.28253784, 0.00148308754, -7.57966669e-07, 2.09470555e-10, -2.16717794e-14, -1088.45772, 5.4532312]]).T
-    Tmax = 3500.000
-    compare = np.array([2.6, 4.7])
-    output = thermochem.Cp_over_R(H_O2,Tmax)
-    assert np.equal(output, compare).all() == 1, 'Unexpected outcome'
 
-## testing for argument number
-def test_reaction_Cp_over_R_argument_number():
+def test_H_over_RT_bad_result():
+    H_O2 = np.array([[2.50000001, -2.30842973e-11, 1.61561948e-14, -4.73515235e-18, 4.98197357e-22, 25473.6599, -0.44668291], [3.28253784, 0.00148308754, -7.57966669e-07, 2.09470555e-10, -2.16717794e-14, -1088.45772, 5.4532312]]).T
+
+    Tmax = 3500.000
+    compare = np.array([3.59508857, 4.26676078, 5.98138715, 6.48213553, -2.73437561])
+    reactions.get_nasa_coeffs = H_O2
+    output = reactions.H_over_RT()
+    assert np.equal(output, compare).any() == False, 'Unexpected outcome'
+
+# testing for argument number
+
+
+def test_reaction_H_over_RT_argument_number():
     try:
-        chemkin.Reaction.Cp_over_R(10**4, 10**2, 10**7)
+        chemkin.Reaction.H_over_RT(10**4, 10**2, 10**7)
     except TypeError as err:
         assert type(err) == TypeError, 'Wrong args number'
 
-## testing for type errors
-def test_reaction_Cp_over_R_T_text():
-    try:
-        chemkin.Reaction.Cp_over_R(('panama'))
-    except Exception as err:
-        assert type(err) == TypeError, 'Bad args format'
+#Function: S_over_R
 
-def test_reaction_Cp_over_R_T_negative():
-    try:
-        chemkin.Reaction.Cp_over_R(-10**2)
-    except Exception as err:
-        assert type(err) == TypeError, 'Bad args format'
+# testing for correct values
 
-def test_reaction_Cp_over_R_T_flow():
+
+def test_S_over_R_result():
+    H_O2 = np.array([[2.50000001, -2.30842973e-11, 1.61561948e-14, -4.73515235e-18, 4.98197357e-22, 25473.6599, -0.44668291], [3.28253784, 0.00148308754, -7.57966669e-07, 2.09470555e-10, -2.16717794e-14, -1088.45772, 5.4532312]]).T
+
+    Tmax = 3500.000
+    compare = np.array([3.69508857, 4.06676078, 5.08138715, 6.28213553, -2.93437561])
+    reactions.get_nasa_coeffs = H_O2
+    output = reactions.S_over_R()
+    assert np.equal(output, compare).all() == 0, 'Expected outcome'
+
+
+def test_S_over_R_bad_result():
+    H_O2 = np.array([[2.50000001, -2.30842973e-11, 1.61561948e-14, -4.73515235e-18, 4.98197357e-22, 25473.6599, -0.44668291], [3.28253784, 0.00148308754, -7.57966669e-07, 2.09470555e-10, -2.16717794e-14, -1088.45772, 5.4532312]]).T
+
+    Tmax = 3500.000
+    compare = np.array([3.59508857, 4.26676078, 5.98138715, 6.48213553, -2.73437561])
+    reactions.get_nasa_coeffs = H_O2
+    output = reactions.S_over_R()
+    assert np.equal(output, compare).any() == False, 'Unexpected outcome'
+
+# testing for argument number
+
+
+def test_reaction_S_over_R_argument_number():
     try:
-        chemkin.Reaction.Cp_over_R(float('inf'))
-    except Exception as err:
-        assert type(err) == TypeError, 'Bad args format'
+        chemkin.Reaction.S_over_R(10**4, 10**2, 10**7)
+    except TypeError as err:
+        assert type(err) == TypeError, 'Wrong args number'
