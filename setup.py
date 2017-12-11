e0626b6c393b1a8a6085642a65c2011b9e42fdbb
Benjamin Rafetto <brafetto@g.harvard.edu>
2017-12-11 01:00:01 -0500

    Added new tests for history files, along with dependency requirements of pymysql

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.8',
+    version='1.1.0',
 
     description='A simple chemical kinetics library',
     long_description=long_description,
@@ -79,7 +79,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=['peppercorn'],
+    install_requires=['pymysql'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
